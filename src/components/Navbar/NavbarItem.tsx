c1c9b061b090b5407ab28382129c6249e07c9a83
hozayves <muhozayves2014@gmail.com>
2024-10-23 16:28:26 +0200

    Resolve active nav link

--- src/components/Navbar/NavbarItem.tsx
+++ src/components/Navbar/NavbarItem.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 import { NavMenuItem } from "@/constant/navbar";
 
 interface NavbarItemProps {
@@ -8,10 +9,16 @@ interface NavbarItemProps {
 }
 
 export default function NavbarItem({ item }: NavbarItemProps) {
+    const pathname = usePathname();
+    const isActive = pathname === item.link;
 
     return (
         <li>
-            <Link href={item.link} className="border rounded-md p-1 bg-violet-200 hover:bg-violet-300 transition-all delay-50 ease-in-out w-[40px] h-[40px] flex justify-center items-center">
+            <Link
+                href={item.link}
+                className={`border rounded-md p-1 transition-all delay-50 ease-in-out w-[40px] h-[40px] flex justify-center items-center ${isActive ? 'bg-violet-400' : 'bg-violet-200 hover:bg-violet-300'
+                    }`}
+            >
                 <Image
                     src={item.image}
                     alt={item.name}
