c0f78665490c4219251bc6f810c761949d58a97b
hozayves <muhozayves2014@gmail.com>
2024-10-24 15:18:48 +0200

    Caught unexpected errors

---
+++ src/app/customer/error.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+
+export default function Error({ error, reset }: { error: Error, reset: () => void }) {
+    useEffect(() => {
+        console.error(error);
+    }, [error]);
+    return <div className="flex flex-col items-center justify-center h-screen">
+        <h2 className="text-2xl font-bold">Something went wrong!</h2>
+        <button onClick={() => reset()} className="bg-violet-500 text-white px-4 py-2 rounded-md">Try again</button>
+    </div>;
+}
